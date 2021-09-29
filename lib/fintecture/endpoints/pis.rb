3077b21a22bf4befe465a879e58f529850a8ff75
Crouzicien <crouzicien@gmail.com>
2021-09-29 14:28:38 +0200

    Endpoints updated for new routes

--- lib/fintecture/endpoints/pis.rb
+++ lib/fintecture/endpoints/pis.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 module Fintecture
   module Api
     module Endpoints
       module Pis
-        PIS = 'pis/v2'
-        PISPROVIDER = "#{PIS}/provider"
-        PISCUSTOMER = "#{PIS}/customer"
-        REQUEST_TO_PAY = "#{PIS}/request-to-pay"
-        PAYMENTS = "#{PIS}/payments"
-        CONNECT = "#{PIS}/connect"
+        INITIATE = 'pis/v2/provider'
+        REFUND = 'pis/v2/refund'
+        REQUEST_TO_PAY = 'pis/v2/request-to-pay'
+        PAYMENTS = 'pis/v2/payments'
+        CONNECT = 'pis/v2/connect'
+        SETTLEMENTS = 'pis/v2/settlements'
       end
     end
   end
