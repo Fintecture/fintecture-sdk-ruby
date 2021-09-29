340951b8241d7cd2580d00d34874026c220ef502
Crouzicien <crouzicien@gmail.com>
2021-09-29 14:40:25 +0200

    Using the client instance instead of the Fintecture module
    Using the client instance instead of the Fintecture module for sessions informations (app_id/environment)

--- lib/fintecture/api/auth/authentication.rb
+++ lib/fintecture/api/auth/authentication.rb
@@ -1,25 +1,27 @@
+# frozen_string_literal: true
+
 require 'json'
 require 'faraday'
 
 module Fintecture
   class Authentication
     class << self
-
       def authorize(redirect_uri, state = nil)
         query_string = "?#{{
-            response_type: 'code',
-            app_id: Fintecture.app_id,
-            redirect_uri: redirect_uri,
-            state: state
-        }.map{|key, value| "#{key}=#{value}"}.join('&')}"
+          response_type: 'code',
+          app_id: Fintecture.app_id,
+          redirect_uri: redirect_uri,
+          state: state
+        }.map { |key, value| "#{key}=#{value}" }.join('&')}"
 
         ::Faraday.get "#{token_authorize_endpoint}#{query_string}"
       end
 
-      def get_access_token(auth_code =  nil)
+      def get_access_token(client, auth_code = nil)
+        @client = client
         body = access_token_data auth_code
-        
-        Fintecture::Faraday::Authentication::Connection.post url: access_token_url, req_body: body
+
+        Fintecture::Faraday::Authentication::Connection.post url: access_token_url, req_body: body, client: client
       end
 
       def refresh_token(refresh_token)
@@ -31,7 +33,7 @@ module Fintecture
       private
 
       def base_url
-        Fintecture::Api::BaseUrl::FINTECTURE_OAUTH_URL[Fintecture.environment.to_sym]
+        Fintecture::Api::BaseUrl::FINTECTURE_OAUTH_URL[@client.environment.to_sym]
       end
 
       def token_authorize_endpoint
@@ -47,17 +49,17 @@ module Fintecture
       end
 
       def access_token_data(auth_code)
-        data =  {
-            scope: 'PIS',
-            app_id: Fintecture.app_id,
-            grant_type: 'client_credentials'
+        data = {
+          scope: 'PIS',
+          app_id: @client.app_id,
+          grant_type: 'client_credentials'
         }
 
         if auth_code
           data = {
-              scope: 'AIS',
-              code: auth_code,
-              grant_type: 'authorization_code'
+            scope: 'AIS',
+            code: auth_code,
+            grant_type: 'authorization_code'
           }
         end
 
@@ -66,11 +68,10 @@ module Fintecture
 
       def refresh_token_data(refresh_token)
         {
-            grant_type: 'refresh_token',
-            refresh_token: refresh_token
+          grant_type: 'refresh_token',
+          refresh_token: refresh_token
         }
       end
-
     end
   end
 end
