1d9b329a8860406e1ef809db3de29da7cf5dbc24
Crouzicien <crouzicien@gmail.com>
2021-09-29 14:37:07 +0200

    Adding different requires

--- lib/fintecture.rb
+++ lib/fintecture.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'logger'
 require 'uri'
 require 'faraday'
 
-#ToRemove
+# ToRemove
 require 'openssl'
 require 'cgi'
 
@@ -10,9 +12,12 @@ require 'cgi'
 require 'fintecture/version'
 
 # Modules
-require 'fintecture/api/pis/connect'
-require 'fintecture/api/pis/pis'
+# require 'fintecture/connect'
 require 'fintecture/api/auth/authentication'
+
+# Clients
+require 'fintecture/pis_client'
+require 'fintecture/ais_client'
 
 # Utilities
 require 'fintecture/utils/crypto'
@@ -20,64 +25,43 @@ require 'fintecture/utils/crypto'
 # Endpoints
 require 'fintecture/base_url'
 require 'fintecture/endpoints/authentication'
+require 'fintecture/endpoints/ais'
+require 'fintecture/endpoints/pis'
+require 'fintecture/endpoints/ressources'
 
 # Connections
 require 'fintecture/faraday/authentication/connection'
 
-
 module Fintecture
   @log_level = nil
   @logger = nil
-  @environment = 'sandbox'
 
-  ENVIRONMENTS = %w[local test sandbox production].freeze
 
   class << self
-    attr_accessor :app_id, :app_secret, :private_key
-
-    def environment=(environment)
-      environment = environment.downcase
-
-      raise "#{environment} not a valid environment, options are [#{ENVIRONMENTS.join(', ')}]" unless ENVIRONMENTS.include?(environment)
-
-      @environment = environment
-    end
-
-    def environment
-      @environment
-    end
+    attr_accessor :logger
+    attr_reader :log_level
 
     # Logging
     LEVEL_DEBUG = Logger::DEBUG
     LEVEL_ERROR = Logger::ERROR
     LEVEL_INFO = Logger::INFO
 
-    def log_level
-      @log_level
-    end
-
     def log_level=(val)
-      if val == "debug"
+      case val
+      when 'debug'
         val = LEVEL_DEBUG
-      elsif val == "info"
+      when 'info'
         val = LEVEL_INFO
       end
 
       if !val.nil? && ![LEVEL_DEBUG, LEVEL_ERROR, LEVEL_INFO].include?(val)
         raise ArgumentError, 'log_level should only be set to `nil`, `debug` or `info`'
       end
+
       @log_level = val
     end
-
-    def logger
-      @logger
-    end
-
-    def logger=(val)
-      @logger = val
-    end
-
   end
 end
 
-Fintecture.log_level = ENV["FINTECTURE_LOG"] unless ENV["FINTECTURE_LOG"].nil?
+Fintecture.log_level = ENV['FINTECTURE_LOG'] unless ENV['FINTECTURE_LOG'].nil?
+# TODO Mettre a jour la gem sur le site de package
