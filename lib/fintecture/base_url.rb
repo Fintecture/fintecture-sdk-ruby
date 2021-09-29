92e3baed274bc6f27efc273cfaca4fc8dc2736bb
Crouzicien <crouzicien@gmail.com>
2021-09-29 15:34:49 +0200

    Update uri production and sandbox for oauth

--- lib/fintecture/base_url.rb
+++ lib/fintecture/base_url.rb
@@ -5,8 +5,8 @@ module Fintecture
       FINTECTURE_OAUTH_URL = {
           local: 'http://localhost:3030/oauth',
           test: 'https://oauth-sandbox-test.fintecture.com/oauth',
-          sandbox: 'https://api-sandbox-test.fintecture.com/oauth',
-          production: 'https://oauth.fintecture.com/oauth'
+          sandbox: 'https://api-sandbox.fintecture.com/oauth',
+          production: 'https://api.fintecture.com/oauth'
       }
 
       FINTECTURE_API_URL = {
