c9dee50bad87dfe7aca40560de9b6293026ef95d
Crouzicien <crouzicien@gmail.com>
2021-09-29 14:42:19 +0200

    Using the client instance instead of the Fintecture & move additional headers merge
    Using the client instance instead of the Fintecture module for sessions informations (app_id/environment)
    Move additional headers merge from secure_headers to req_headers function

--- lib/fintecture/faraday/authentication/connection.rb
+++ lib/fintecture/faraday/authentication/connection.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'base64'
 require 'faraday'
 require 'uri'
@@ -9,112 +11,108 @@ module Fintecture
     module Authentication
       class Connection
         class << self
-
           def connection(url)
             ::Faraday.new(url: url) do |faraday|
               faraday.request :url_encoded
-              faraday.adapter  ::Faraday.default_adapter
+              faraday.adapter ::Faraday.default_adapter
             end
           end
 
-          def post(url:, req_body: nil, custom_content_type: nil, bearer: nil, secure_headers: false, additional_headers: nil)
+          def post(url:, req_body: nil, client: nil, custom_content_type: nil, bearer: nil, secure_headers: false, additional_headers: nil)
+            @client = client
             conn = connection(url)
 
             res = conn.post do |req|
               req.options.params_encoder = Faraday::DisabledEncoder
-              req.headers = req_headers(custom_content_type, bearer, secure_headers, additional_headers, method: 'post', body: req_body, url: url)
+              req.headers = req_headers(custom_content_type, bearer, secure_headers, additional_headers,
+                                        method: 'post', body: req_body, url: url)
               req.body = req_body
             end
 
             !res.success? ? Fintecture::ApiException.error(res) : res
           end
 
-          def get(url:, req_body: nil, custom_content_type: nil, bearer: nil, secure_headers: false, additional_headers: nil)
+          def get(url:, req_body: nil, client: nil, custom_content_type: nil, bearer: nil, secure_headers: false, additional_headers: nil)
+            @client = client
             conn = connection(url)
 
             res = conn.get do |req|
               req.options.params_encoder = Faraday::DisabledEncoder
-              req.headers = req_headers(custom_content_type, bearer, secure_headers, additional_headers, method: 'get', url: url)
+              req.headers = req_headers(custom_content_type, bearer, secure_headers, additional_headers, method: 'get',
+                                                                                                         url: url)
               req.body = req_body
             end
 
             !res.success? ? Fintecture::ApiException.error(res) : res
           end
 
-          def req_headers(custom_content_type, bearer, secure_headers, additional_headers, method: '', body: {}, url:)
+          def req_headers(custom_content_type, bearer, secure_headers, additional_headers, url:, method: '', body: {})
+            client_token = Base64.strict_encode64("#{@client.app_id}:#{@client.app_secret}")
 
-            client_token = Base64.strict_encode64("#{Fintecture.app_id}:#{Fintecture.app_secret}")
-
-            {
-                'Accept' => 'application/json',
-                'User-Agent' => "Fintecture Ruby SDK v #{Fintecture::VERSION}",
-                'Authorization' => bearer ? bearer : "Basic #{client_token}",
-                'Content-Type' => custom_content_type ? custom_content_type : 'application/x-www-form-urlencoded',
-            }.merge(secure_headers ? req_secure_headers(additional_headers, body: body, url: url, method: method) : {})
-
+            headers = {
+              'Accept' => 'application/json',
+              'User-Agent' => "Fintecture Ruby SDK v #{Fintecture::VERSION}",
+              'Authorization' => bearer || "Basic #{client_token}",
+              'Content-Type' => custom_content_type || 'application/x-www-form-urlencoded'
+            }
+            headers = headers.merge(secure_headers ? req_secure_headers(additional_headers, body: body, url: url, method: method) : {})
+            headers = headers.merge(additional_headers) unless additional_headers.nil?
+            headers
           end
 
           def req_secure_headers(additional_headers, body: {}, url: '', method: '')
-            raise Fintecture::ValidationException.new('additional_headers must be an object') if additional_headers != nil && !additional_headers.is_a?(Hash) 
-          
-            payload = ( body.class.name == 'String' ? body : body.to_s )
+            if !additional_headers.nil? && !additional_headers.is_a?(Hash)
+              raise Fintecture::ValidationException, 'additional_headers must be an object'
+            end
+
+            payload = (body.instance_of?(String) ? body : body.to_s)
             path_name = URI(url).path
             search_params = URI(url).query
-
 
             request_target = search_params ? "#{method.downcase} #{path_name}?#{search_params}" : "#{method.downcase} #{path_name}"
             date = Fintecture::Utils::Date.header_time.to_s
             digest = load_digest(payload)
             x_request_id = Fintecture::Utils::Crypto.generate_uuid
 
-
             headers = {
-                'Date' => date,
-                'X-Request-ID' => x_request_id
+              'Date' => date,
+              'X-Request-ID' => x_request_id
             }.merge(payload ? digest : {})
 
-            # Add additional_headers if exists
-            headers = headers.merge(additional_headers) if additional_headers != nil
 
 
-            headers['Signature'] = Fintecture::Utils::Crypto.create_signature_header({'(request-target)' => request_target}.merge(headers))
+            headers['Signature'] =
+              Fintecture::Utils::Crypto.create_signature_header(
+                { '(request-target)' => request_target }.merge(headers), @client
+              )
             headers
           end
 
           def load_digest(payload)
-            {'Digest' => "SHA-256=#{Fintecture::Utils::Crypto.hash_base64(payload)}"}
+            { 'Digest' => "SHA-256=#{Fintecture::Utils::Crypto.hash_base64(payload)}" }
           end
-
         end
       end
     end
-
 
     module DisabledEncoder
       class << self
         extend Forwardable
         def_delegators :'Faraday::Utils', :escape, :unescape
       end
-  
 
       def self.encode(params)
         return nil if params.nil?
-  
-        query_string = "#{params.map{|key, value| "#{key}=#{value}"}.join('&')}"
-        
-      end
-  
 
-  
+        query_string = params.map { |key, value| "#{key}=#{value}" }.join('&').to_s
+      end
+
       class << self
         attr_accessor :sort_params
       end
-  
+
       # Useful default for OAuth and caching.
       @sort_params = true
     end
   end
 end
-
-
-
