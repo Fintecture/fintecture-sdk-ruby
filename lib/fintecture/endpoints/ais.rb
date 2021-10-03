f150efbc8a5862358892e41612f7b0890daa373d
Crouzicien <crouzicien@gmail.com>
2021-10-03 18:10:49 +0200

    Add new endpoints

--- lib/fintecture/endpoints/ais.rb
+++ lib/fintecture/endpoints/ais.rb
@@ -4,7 +4,13 @@ module Fintecture
   module Api
     module Endpoints
       module Ais
+        # TODO faire plus complet, avec un mot clef a remplacer
         CONNECT = 'ais/v2/connect'
+        ACCOUNTS = 'ais/v1/customer'
+        TRANSACTIONS = 'ais/v1/customer'
+        ACCOUNTHOLDERS = 'ais/v1/customer'
+        CUSTOMER = 'ais/v1/customer'
+        AUTHORIZE = 'ais/v1/provider'
       end
     end
   end
