ecaa279a78408afe81ff7acc5b1845cebba29148
Crouzicien <crouzicien@gmail.com>
2021-10-03 18:10:24 +0200

    Add accounts/transactions/account_holders/delete_customer/authorize/authorize_decoupled routes

--- lib/fintecture/ais_client.rb
+++ lib/fintecture/ais_client.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 require 'fintecture/api/ais/connect'
+require 'fintecture/api/ais/accounts'
+require 'fintecture/api/ais/transactions'
+require 'fintecture/api/ais/account_holders'
+require 'fintecture/api/ais/delete_customer'
+require 'fintecture/api/ais/authorize'
+require 'fintecture/api/ais/authorize_decoupled'
 
 module Fintecture
   class AisClient
@@ -40,9 +46,40 @@ module Fintecture
       body
     end
 
+    def accounts(customer_id:, account_id: nil, remove_nulls: nil, withBalances: nil)
+      res = Fintecture::Ais::Accounts.get self, customer_id, account_id, remove_nulls, withBalances
 
+      JSON.parse res.body
+    end
 
+    def transactions(customer_id:, account_id:, remove_nulls: nil, convert_dates: nil, filters: nil)
+      res = Fintecture::Ais::Transactions.get self, customer_id, account_id, remove_nulls, convert_dates, filters
 
+      JSON.parse res.body
+    end
 
+    def account_holders(customer_id:, remove_nulls: nil)
+      res = Fintecture::Ais::AccountHolders.get self, customer_id, remove_nulls
+
+      JSON.parse res.body
+    end
+
+    def delete_customer(customer_id:)
+      res = Fintecture::Ais::DeleteCustomer.delete self, customer_id
+
+      JSON.parse res.body
+    end
+
+    def authorize(app_id_auth: false, provider_id:, redirect_uri:, state: nil, x_psu_id: nil, x_psu_ip_address: nil)
+      res = Fintecture::Ais::Authorize.get self, app_id_auth, provider_id, redirect_uri, state, x_psu_id, x_psu_ip_address
+
+      JSON.parse res.body
+    end
+
+    def authorize_decoupled(app_id_auth: false, provider_id:, polling_id:)
+      res = Fintecture::Ais::AuthorizeDecoupled.get self, app_id_auth, provider_id, polling_id
+
+      JSON.parse res.body
+    end
   end
 end
---
+++ lib/fintecture/api/ais/authorize_decoupled.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require 'base64'
+require 'json'
+require 'faraday'
+require 'fintecture/utils/validation'
+require 'fintecture/exceptions'
+require 'fintecture/utils/date'
+require 'fintecture/utils/constants'
+
+module Fintecture
+  module Ais
+    class AuthorizeDecoupled
+      class << self
+        # ------------ PUBLIC METHOD ------------
+        def get(client, app_id_auth, provider_id, polling_id)
+          @client = client
+
+          # Do the request
+          _request app_id_auth, provider_id, polling_id
+        end
+
+        private
+
+        # ------------ REQUEST ------------
+        def _request(app_id_auth, provider_id, polling_id)
+          # Get the url request
+          url = _endpoint provider_id, polling_id
+
+          # Build uri params
+          query_string = ""
+
+          params = {}
+          params['response_type'] = "code" if app_id_auth
+          params['model'] = "decoupled"
+    
+          query_string = "?#{params.map { |key, value| "#{key}=#{value}" }.join('&')}"
+
+          # Build additional headers
+          additional_headers = {}
+          additional_headers['app_id'] = @client.app_id if app_id_auth
+
+
+          puts query_string
+          # Do connect request
+          Fintecture::Faraday::Authentication::Connection.get(
+            url: url + query_string,
+            client: @client,
+            custom_content_type:  'application/json',
+            bearer: "Bearer #{@client.token}",
+            secure_headers: true,
+            additional_headers: additional_headers,
+            disableAuthorization: app_id_auth ? true : false
+          )
+        end
+
+
+
+        # ------------ API ENDPOINT ------------
+        def _endpoint (provider_id, polling_id)
+          "#{_api_base_url}/#{Fintecture::Api::Endpoints::Ais::AUTHORIZE}/#{provider_id}/authorize/decoupled/#{polling_id}"
+        end
+
+        # ------------ BASE URL ------------
+        def _api_base_url
+          Fintecture::Api::BaseUrl::FINTECTURE_API_URL[@client.environment.to_sym]
+        end
+
+        # ------------ TOOLS ------------
+        def as_json(element)
+          return JSON(element.to_json) if element.is_a? Hash
+
+          begin
+            element.as_json
+          rescue NoMethodError
+            raise Fintecture::ValidationException,
+                  "invalid parameter format, the parameter should be a Hash or an Object Model instead a #{element.class.name}"
+          end
+        end
+      end
+    end
+  end
+end
